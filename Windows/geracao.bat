762fdafd861f2c4516e2b9c6c828ac621b935a9d
Jean Henrique Ferreira <jeanferreira@alunos.utfpr.edu.br>
2016-06-18 21:00:54 -0300

    geração treinamento

--- Windows/geracao.bat
+++ Windows/geracao.bat
@@ -8,5 +8,6 @@ cd ..
 mkdir Positivas
 mkdir Negativas
 java -jar CropAndPaintBlack.jar -file "Imagens.txt" -lote 5
-createsamples.exe -info positivas.txt -vec vector.vec -num 204 -w 24 -h 24
+Data\createsamples.exe -info positivas.txt -vec vector.vec -num 204 -w 24 -h 24
+Data\haartraining.exe -data Cascades -vec vector.vec -bg negativas.txt -npos 200 -nneg 2000 -nstages 15 -mem 1024 -mode ALL -w 24 -h 24 
 pause
